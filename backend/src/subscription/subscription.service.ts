c885f9943a5c4190d84e10843621a8cfc7288ace
Dao Manh Tung <daomanhtung4102003@gmail.com>
2026-01-17 01:02:10 +0700

    fix: remove unused import

--- backend/src/subscription/subscription.service.ts
+++ backend/src/subscription/subscription.service.ts
@@ -2,7 +2,6 @@ import {
   Injectable,
   NotFoundException,
   ForbiddenException,
-  BadRequestException,
 } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { CreateSubscriptionDto } from './dto/create-subscription.dto';
